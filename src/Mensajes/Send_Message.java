5973b9648acfeab4966e93706203c8dca8c53294
joanjuz <62675444+joanjuz@users.noreply.github.com>
2020-10-21 15:49:08 -0600

    Update Send_Message.java

--- src/Mensajes/Send_Message.java
+++ src/Mensajes/Send_Message.java
@@ -11,6 +11,8 @@ import javafx.scene.control.Button;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 import java.net.Socket;
@@ -18,6 +20,7 @@ import java.util.ArrayList;
 import java.util.concurrent.ConcurrentLinkedDeque;
 
 public class Send_Message{
+    private static Logger log = LoggerFactory.getLogger(Send_Message.class);
 
     public static String messages = "";
     public static int puerto;
@@ -31,39 +34,56 @@ public class Send_Message{
 
 
     public static void send(TextField Caja, TextArea Chat) throws IOException, ClassNotFoundException {
-        if(client2 == 0) {
-            String message = "No has seleccionado un destinatario" + "\n";
-            Chat.setText(message);
-            Caja.setText("");
-        }else if(localclient==0){
-            localclient = Main.port_();
-        }else{
-            messages = Caja.getText();
-            Client.set_message(messages);
-            Caja.setText("");
-            if(Client.Chatbox().equals("")) {
-                Chat.setText(backmen);
-            }else{
-                backmen = Client.Chatbox();
-                Chat.setText(Client.Chatbox());
+        try {
+            log.debug("Enviando mensaje...");
+            if (client2 == 0) {
+                log.error("No se ha seleccionado un destinatario");
+                String message = "No has seleccionado un destinatario" + "\n";
+                Chat.setText(message);
+                Caja.setText("");
+                throw new IllegalArgumentException("No has determinado el destinatario");
+            } else if (localclient == 0) {
+                log.debug("Registrando cliente...");
+                localclient = Main.port_();
+            } else {
+                messages = Caja.getText();
+                try {
+                    Client.set_message(messages);
+                    Caja.setText("");
+                }catch (Exception e){
+                    log.error(e.getMessage(),e);
+                }
+                if (Client.Chatbox().equals("")) {
+                    Chat.setText(backmen);
+                } else {
+                    backmen = Client.Chatbox();
+                    Chat.setText(Client.Chatbox());
+                }
             }
-
-
-
+        }catch (IllegalArgumentException e){
+            log.error(e.getMessage(),e);
         }
     }
     public static void update(TextArea Chat){
-        Chat.setText(Client.Chatbox());
+        if (Client.Chatbox()==null){
+            log.info("No hay mensages registradors");
+        }else {
+            Chat.setText(Client.Chatbox());
+        }
     }
     public static void set_port(int puerto, Button destino,TextArea Chat) throws IOException {
         if(destino.getText().equals("Añadir")) {
-            Sendto.display(puerto);
-            String e = Sendto.answer;
-            client2 = Integer.parseInt(Sendto.answer);
-            localclient = puerto;
-            destino.setText(e);
-            Client.set_otherport(client2);
-            Chat.setText("");
+            try {
+                Sendto.display(puerto);
+                String e = Sendto.answer;
+                client2 = Integer.parseInt(Sendto.answer);
+                localclient = puerto;
+                destino.setText(e);
+                Client.set_otherport(client2);
+                Chat.setText("");
+            }catch (Exception e){
+                log.error(e.getMessage(),e);
+            }
 
         }else{
             send.puerto = Integer.parseInt(destino.getText());
